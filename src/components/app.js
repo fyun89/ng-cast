36c50b5f75c07831f237e2bd04538994d1226467
fyun89 <fyun89@gmail.com>
2018-05-11 22:16:50 -0700

    Completed up to Hooking components together for click events to work

--- src/components/app.js
+++ src/components/app.js
@@ -1,16 +1,15 @@
 angular.module('video-player')
 
-.component('app', {
-  // TODO
-  controller: function() {
-    this.videos = window.exampleVideoData,
-    this.currentVideo = window.exampleVideoData[0],
-    this.setCurrentVideo = (function(video) {
-      console.log('clicked:', video);
-      //debugger;
-      this.currentVideo = video;
-      //$ctrl.currentVideo = video
-    }).bind(this);
-  },
-  templateUrl: '/src/templates/app.html'
-});
+  .component('app', {
+    // TODO
+    controller: function() {
+      this.videos = window.exampleVideoData,
+      this.currentVideo = window.exampleVideoData[0],
+      this.setCurrentVideo = (function(video) {
+        this.currentVideo = video;
+        console.log('clicked:', this.currentVideo);
+        //$ctrl.currentVideo = video
+      }).bind(this);
+    },
+    templateUrl: '/src/templates/app.html'
+  });
--- src/components/videoPlayer.js
+++ src/components/videoPlayer.js
@@ -1,10 +1,14 @@
 angular.module('video-player')
 
-.component('videoPlayer', {
-  // TODO
-  controller: function() {
-    this.currentVideo = window.exampleVideoData[0],
-    this.url = "https://www.youtube.com/embed/" + this.currentVideo.id.videoId
-  },
-  templateUrl: 'src/templates/videoPlayer.html'
-});
+  .component('videoPlayer', {
+    // TODO
+    controller: function() {
+      //this.currentVideo = window.exampleVideoData[0],
+      //console.log($ctrl.currentVideo)
+      //this.url = "https://www.youtube.com/embed/" + this.currentVideo.id.videoId
+    },
+    templateUrl: 'src/templates/videoPlayer.html',
+    bindings: {
+      currentVideo: '<'
+    }
+  });
